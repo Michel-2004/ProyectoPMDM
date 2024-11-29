b7766322ab905f3f6e7cede16442aef3c9fefb77
agu204 <aarigon742@g.educaand.es>
2024-11-29 19:38:00 +0100

    Update

--- app/src/main/java/com/example/proyectopmdm/jugadores/jugadoresView.kt
+++ app/src/main/java/com/example/proyectopmdm/jugadores/jugadoresView.kt
@@ -1,21 +1,21 @@
 package com.example.proyectopmdm.jugadores
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.navigation.NavController
-import coil3.compose.AsyncImage
-import coil3.request.ImageRequest
-import coil3.request.crossfade
-import java.lang.reflect.Modifier
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 
 @Composable
 fun jugadoresView(viewModel: JugadoresViewModel) {
@@ -25,28 +25,79 @@ fun jugadoresView(viewModel: JugadoresViewModel) {
         viewModel.fetchJugadores()
     }
 
-    Column {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
         if (jugadores.isEmpty()) {
-            Text(text = "Cargando...")
-        }
-        else{
-            LazyColumn{
-                items(jugadores) { jugadores ->
-                    Text(text = jugadores.numero)
-                    Text(text = jugadores.nombre)
-                    Text(text = jugadores.posicion)
-                    Text(text = jugadores.nacionalidad)
-                    Text(text = jugadores.altura)
-                    Text(text = jugadores.valorMercado)
+            Text(
+                text = "Cargando...",
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+        } else {
+            LazyColumn(verticalArrangement = Arrangement.spacedBy(16.dp)) {
+                items(jugadores) { jugador ->
 
-                    AsyncImage(
-                        model = ImageRequest.Builder(LocalContext.current)
-                            .data(jugadores.img.replace("http://", "https://"))
-                            .crossfade(true)
-                            .build(),
-                        contentDescription = jugadores.nombre,
-                        contentScale = ContentScale.Fit
-                    )
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface),
+                        elevation = CardDefaults.cardElevation(8.dp)
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+
+                            AsyncImage(
+                                model = ImageRequest.Builder(LocalContext.current)
+                                    .data(jugador.img.replace("http://", "https://"))
+                                    .crossfade(true)
+                                    .build(),
+                                contentDescription = jugador.nombre,
+                                contentScale = ContentScale.Fit,
+                                modifier = Modifier
+                                    .size(150.dp)
+                                    .padding(bottom = 8.dp)
+                            )
+
+                            
+                            Text(
+                                text = "Número: ${jugador.numero}",
+                                style = MaterialTheme.typography.bodyMedium,
+                                modifier = Modifier.padding(bottom = 4.dp)
+                            )
+                            Text(
+                                text = "Nombre: ${jugador.nombre}",
+                                style = MaterialTheme.typography.bodyMedium,
+                                modifier = Modifier.padding(bottom = 4.dp)
+                            )
+                            Text(
+                                text = "Posición: ${jugador.posicion}",
+                                style = MaterialTheme.typography.bodyMedium,
+                                modifier = Modifier.padding(bottom = 4.dp)
+                            )
+                            Text(
+                                text = "Nacionalidad: ${jugador.nacionalidad}",
+                                style = MaterialTheme.typography.bodyMedium,
+                                modifier = Modifier.padding(bottom = 4.dp)
+                            )
+                            Text(
+                                text = "Altura: ${jugador.altura}",
+                                style = MaterialTheme.typography.bodyMedium,
+                                modifier = Modifier.padding(bottom = 4.dp)
+                            )
+                            Text(
+                                text = "Valor Mercado: ${jugador.valorMercado}",
+                                style = MaterialTheme.typography.bodyMedium,
+                                modifier = Modifier.padding(bottom = 4.dp)
+                            )
+                        }
+                    }
                 }
             }
         }
